37c2f864ea49b9ac6c8169654006184871d10319
solarus2120 <74253809+solarus2120@users.noreply.github.com>
2020-11-17 15:03:33 +0000

    Add files via upload

---
+++ Data.py
@@ -0,0 +1,18 @@
+from Branch.Recovery.People import *
+from Branch.Recovery.Weapons import *
+from Branch.Recovery.Armour import *
+
+Lucien = People("Lucien", "Healthy", "Long Blade", 10, 40, "Swordsman", "Suit of Mail")
+Kitania = People("Kitania", "Healthy", "War Axe", 10, 40, "Myrmidon", "Leather Breastplate")
+Cassandra = People("Cassandra", "Healthy", "Small Blade", 10, 40, "Sneak", "Leather Jack")
+
+LongBlade = Weapons("Long Blade", 10, 2, 0)
+WarAxe = Weapons("War Axe", 20, 3, -1)
+SmallBlade = Weapons("Small Blade", 5, 1, 1)
+
+Shirt = Mail("Mail Shirt", True, "Light", "Torso", 1)
+Hauberk = Mail("Mail Hauberk", True, "Medium", "Torso and Arms", 2)
+Byrnie = Mail("Mail Byrnie", True, "Medium", "Head, Torso and Arms", 3)
+Suit = Mail("Suit of Mail", True, "Heavy", "Head, Torso, Arms and Legs", 4)
+Breastplate = Leather("Leather Breastplate", False, "Light", "Cow", 2)
+Jack = Leather("Leather Jack", False, "Light", "Sheep", 1)
---
+++ People.py
@@ -0,0 +1,59 @@
+import time
+from Branch.Recovery.Data import *
+from Branch.Recovery.Weapons import *
+from Branch.Recovery.Armour import *
+
+
+class People:
+    def __init__(self, name, status, equip, health, energy, rank, armour_equip):
+        self.name = name
+        self.status = status
+        self.equip = equip
+        self.energy = energy
+        self.health = health
+        self.rank = rank
+        self.armour_equip = armour_equip
+
+    def introduce(self):
+        print("Hello. My name is " + self.name)
+        print("I am a {}, and I am armed with {} and {}".format(self.rank, self.equip, self.armour_equip))
+
+    def strike(self, target):
+        print("{} is armed with {} and strikes a mighty blow against {}".format(self.name, self.equip, target.name))
+        time.sleep(0.5)
+
+        wpn = None
+        amr = None
+
+        if self.equip == LongBlade.name:
+            wpn = LongBlade
+        elif self.equip == WarAxe.name:
+            wpn = WarAxe
+        elif self.equip == SmallBlade.name:
+            wpn = SmallBlade
+
+        if target.armour_equip == Shirt.name:
+            amr = Shirt
+        elif target.armour_equip == Hauberk.name:
+            amr = Hauberk
+        elif target.armour_equip == Byrnie.name:
+            amr = Byrnie
+        elif target.armour_equip == Suit.name:
+            amr = Suit
+        elif target.armour_equip == Breastplate.name:
+            amr = Breastplate
+        elif target.armour_equip == Jack.name:
+            amr = Jack
+
+        if not amr.protect_me(amr.protection):
+            target.health -= wpn.damage
+            if target.health > 9:
+                target.status = "Healthy"
+            elif target.health > 5:
+                target.status = "Lightly Wounded"
+            elif target.health > 0:
+                target.status = "Heavily Wounded"
+            else:
+                target.status = "Dead"
+            print("{} suffers an injury and is now {}".format(target.name, target.status))
+            time.sleep(0.5)
