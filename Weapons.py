27d16534c6179989531af6cdbe99397374248153
solarus2120 <74253809+solarus2120@users.noreply.github.com>
2020-11-18 15:01:56 +0000

    Delete Weapons.py
    redundant additional file

--- Weapons.py
+++
@@ -1,6 +0,0 @@
-class Weapons:
-    def __init__(self, name, energy, damage, init_mod):
-        self.name = name
-        self.energy = energy
-        self.damage = damage
-        self.init_mod = init_mod
